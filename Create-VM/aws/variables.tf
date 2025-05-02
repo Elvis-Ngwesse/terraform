ff7dbf3ef8b1d4ae4375db2e39abf93099a59391
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2025-05-02 05:16:16 +0100

    updates

--- Create-VM/aws/variables.tf
+++ Create-VM/aws/variables.tf
@@ -1,36 +1,76 @@
-# variables.tf
+# -----------------------------
+# Environment and Region Variables
+# -----------------------------
 
-# Variable for the EC2 instance machine type
-variable "machine" {
-  description = "The EC2 instance machine type"
-  type        = string
-}
-
-variable "availability_zone" {
-  description = "The availability zone for the EC2 instance"
-  type        = string
-}
-
-# Variable for the SSH key name to access EC2 instances
-variable "ssh_key_name" {
-  description = "The SSH key name to access the EC2 instances"
-  type        = string
-}
-
-# Variable for the AWS region
-variable "aws_region" {
-  description = "The AWS region to deploy the resources"
-  type        = string
-}
-
-# Variable for environment name
+# The environment where the infrastructure is deployed (e.g., dev, prod)
 variable "environment" {
-  description = "Environment for the resources"
+  description = "The environment to deploy resources (e.g., dev, prod)"
   type        = string
 }
 
-# Variable for the AMI ID used for EC2 instances
+# AWS region to deploy the resources
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-west-2"  # Default region if not specified
+}
+
+# The AWS availability zone for the subnets
+variable "availability_zone" {
+  description = "The AWS availability zone for the subnets"
+  type        = string
+}
+
+# -----------------------------
+# EC2 Instance Variables
+# -----------------------------
+
+# AMI ID to use for the EC2 instance
 variable "ami_id" {
-  description = "The AMI ID to use for EC2 instances"
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+# Instance type for the EC2 instance
+variable "machine" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"  # Default to a small instance type
+}
+
+# SSH key pair name for accessing EC2 instances
+variable "ssh_key_name" {
+  description = "The SSH key pair name for accessing EC2 instances"
+  type        = string
+}
+
+# -----------------------------
+# Networking and Security Variables
+# -----------------------------
+
+# The CIDR block for the VPC
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"  # Example CIDR block for the VPC
+}
+
+# The CIDR block for the public subnet
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"  # Example CIDR block for the public subnet
+}
+
+# The CIDR block for the private subnet
+variable "private_subnet_cidr" {
+  description = "The CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"  # Example CIDR block for the private subnet
+}
+
+# The ID of the security group for the EC2 instance
+variable "security_group_id" {
+  description = "The ID of the security group for the EC2 instance"
   type        = string
 }
--- Create-VM/google/variables.tf
+++ Create-VM/google/variables.tf
@@ -1,35 +1,54 @@
+# -----------------------------
+# GCP Environment Variables
+# -----------------------------
+
+# Environment for the resources (e.g., dev, prod)
 variable "environment" {
-  description = "Environment for the resources"
+  description = "Environment for the resources (e.g., dev, prod)"
   type        = string
 }
 
+# The GCP project ID
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
 }
 
+# The GCP region for the resources
 variable "gcp_region" {
   description = "The GCP region"
   type        = string
 }
 
+# The GCP zone where the resources will be deployed
 variable "gcp_zone" {
   description = "The GCP zone"
   type        = string
 }
 
+# -----------------------------
+# GCP Compute Instance Variables
+# -----------------------------
+
+# The machine type for the instances
 variable "machine_gcp" {
-  description = "The machine type for the instances"
+  description = "The machine type for the instances (e.g., e2-medium, n1-standard-1)"
   type        = string
 }
 
+# The image to use for the instances
 variable "image" {
-  description = "The image to use for the instances"
+  description = "The image to use for the instances (e.g., ubuntu-2004-lts)"
   type        = string
 }
 
+# -----------------------------
+# GCP Credentials Variables
+# -----------------------------
+
+# Path to the Google Cloud credentials JSON file (for authentication)
 variable "gcp_credentials_file" {
-  type        = string
   description = "Path to the Google Cloud credentials JSON file"
-  sensitive   = true
+  type        = string
+  sensitive   = true  # This ensures the credentials are not displayed in logs
 }
